ffc524399363acd23758e4c1d1052c9e0793861e
Zanellato Matteo <matteo.zanellato0698@gmail.com>
2019-10-25 20:14:22 +0200

    changed constructors

--- software/Nightmare_test_codeV3/N_structs.h
+++ software/Nightmare_test_codeV3/N_structs.h
@@ -4,15 +4,14 @@
 struct Dleg{
   float CX_LENGHT,FM_LENGHT,TB_LENGHT,START_X_OFFSET,START_Y_OFFSET;
   bool SIDE;
-  Dleg(){}
-  Dleg(float CX_LENGHT, float FM_LENGHT, float TB_LENGHT, float START_X_OFFSET, float START_Y_OFFSET, bool SIDE){
-    this->CX_LENGHT = CX_LENGHT;
-    this->FM_LENGHT = FM_LENGHT;
-    this->TB_LENGHT = TB_LENGHT;
-    this->START_X_OFFSET = START_X_OFFSET;
-    this->START_Y_OFFSET = START_Y_OFFSET;
-    this->SIDE = SIDE;
-  }
+  Dleg() = default;
+  Dleg(float CX_LENGHT, float FM_LENGHT, float TB_LENGHT, float START_X_OFFSET, float START_Y_OFFSET, bool SIDE) :
+    CX_LENGHT(CX_LENGHT),
+    FM_LENGHT(FM_LENGHT),
+    TB_LENGHT(TB_LENGHT),
+    START_X_OFFSET(START_X_OFFSET),
+    START_Y_OFFSET(START_Y_OFFSET),
+    SIDE(SIDE) {}
 };
   
 #endif
