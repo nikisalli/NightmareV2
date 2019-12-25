3463bcf4bc4856c9f2e04b03f094ff55bdb0ef7a
nikisalli <niki.salli@gmail.com>
2019-12-25 12:23:35 +0100

    added some comments to python file

--- software/Nightmare_ros_code/nightmare/src/listener.py
+++ software/Nightmare_ros_code/nightmare/src/listener.py
@@ -15,43 +15,43 @@ from std_msgs.msg import Header
 from geometry_msgs.msg import Quaternion
 from tf.broadcaster import TransformBroadcaster
 
-robot = serial.Serial('/dev/ttyUSB0', 460800)
-imu = serial.Serial('/dev/ttyACM0', 115200)
+robot = serial.Serial('/dev/ttyUSB0', 460800)                                                      #robot serial port
+imu = serial.Serial('/dev/ttyACM0', 115200)                                                        #imu serial port
 
 robot.isOpen()
 
-def fmap(x, in_min, in_max, out_min, out_max):
+def fmap(x, in_min, in_max, out_min, out_max):                                                     #simple linear interpolation function
   return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min
 
-class robot_listener(Thread):
+class robot_listener(Thread):                                                                      #robot listener to parse data coming from the robot
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
         self.start()
     def run(self):
         while 1:
-            if(ord(robot.read()) == 0xAA):
-                if(ord(robot.read()) == 0xBB):
+            if(ord(robot.read()) == 0xAA):                                                         #search for first header byte
+                if(ord(robot.read()) == 0xBB):                                                     #search for second header byte
                     bytes = []
-                    for _ in range(26):
-                        bytes.append(ord(robot.read()))
-                    robot_listener.voltage = fmap(bytes[0],0,255,5,10)
+                    for _ in range(26): 
+                        bytes.append(ord(robot.read()))                                            #one message is made of 26 bytes so read them all
+                    robot_listener.voltage = fmap(bytes[0],0,255,5,10)                             #scale variables to readable data
                     robot_listener.current = fmap(bytes[1],0,255,0,15)
-                    robot_listener.joints = bytes[2:26]
+                    robot_listener.joints = bytes[2:26]                                            #the last 24 bytes of the message are servo angles
 
-class imu_listener(Thread):
+class imu_listener(Thread):                                                                        #imu listener to parse imu data
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
         self.start()
     def run(self):
         while 1:
-            if(ord(imu.read()) == 0xBB):
+            if(ord(imu.read()) == 0xBB):                                                           #search for the header
                 imu.read()
                 bytes = []
                 for _ in range(3):
                     bytes.append(ord(imu.read()))
-                imu_listener.roll = fmap(bytes[0],0,255,-180,180)
+                imu_listener.roll = fmap(bytes[0],0,255,-180,180)                                  #scale bytes to readable data
                 imu_listener.pitch = fmap(bytes[1],0,255,-180,180)
                 imu_listener.yaw = fmap(bytes[2],0,255,-180,180)
 
@@ -60,16 +60,17 @@ class nightmare_node():
         self.degrees2rad = math.pi/180.0
 
         # Get values from launch file
-        self.rate = rospy.get_param('~rate', 100.0)  # the rate at which to publish the transform
-        # Static transform between sensor and fixed frame: x, y, z, roll, pitch, yaw
-        # <rosparam param="static_transform">[0, 0, 0, 0, 0, 0]</rosparam>
-        self.static_transform = rospy.get_param('~static_transform', [0, 0, 0, 0, 0, 0])
-        self.topic_name = rospy.get_param('~topic_name', "/imu")
-        self.fixed_frame = rospy.get_param('~fixed_frame', "world")
-        self.frame_name = rospy.get_param('~frame_name', "imu")
-        self.publish_transform = rospy.get_param('~publish_transform', False)
 
-        self.pub_imu = rospy.Publisher("nightmare/imu", Imu, queue_size=1)
+        self.rate = rospy.get_param('~rate', 100.0)                                                #the rate at which to publish the transform
+
+        
+        self.static_transform = rospy.get_param('~static_transform', [0, 0, 0, 0, 0, 0])           #static transform between sensor and fixed frame: r, p, y, x, y, z
+        self.topic_name = rospy.get_param('~topic_name', "/imu")                                   #set imu topic name
+        self.fixed_frame = rospy.get_param('~fixed_frame', "world")                                #set fixed frame to apply the transform
+        self.frame_name = rospy.get_param('~frame_name', "imu")                                    #set frame name
+        self.publish_transform = rospy.get_param('~publish_transform', False)                      #set true in lunch file if the transform is needed
+
+        self.pub_imu = rospy.Publisher("nightmare/imu", Imu, queue_size=1)                         #create topics name, type and data persistance
         self.pub_vol = rospy.Publisher("nightmare/battery/voltage", Float32, queue_size=1)
         self.pub_cur = rospy.Publisher("nightmare/battery/current", Float32, queue_size=1)
         self.pub_jnt = rospy.Publisher("nightmare_v2/joint_states", JointState, queue_size=10)
@@ -149,10 +150,6 @@ class nightmare_node():
         res = robot_listener.joints
         tmp = [fmap(i,0,255,120,-120)*self.degrees2rad for i in res] 
 
-        #self.jnt_msg.position = [tmp[9], tmp[12], tmp[15], tmp[18], tmp[21], tmp[0], tmp[3], tmp[6], tmp[13],
-        #                         tmp[16], tmp[19], tmp[22], tmp[10], tmp[7], tmp[4], tmp[1], tmp[11], tmp[8],
-        #                         tmp[5], tmp[2], tmp[23], tmp[20], tmp[17], tmp[14], 0, 0, 0]
-        
         self.jnt_msg.position = [-tmp[12], -tmp[9], -tmp[6], -tmp[3], -tmp[0], -tmp[21], -tmp[18], -tmp[15], tmp[10],
                                  tmp[7], tmp[4], tmp[1], tmp[13], tmp[16], tmp[19], tmp[22], tmp[14], tmp[17],
                                  tmp[20], tmp[23], tmp[2], tmp[5], tmp[8], tmp[11], 0, 0, 0]
