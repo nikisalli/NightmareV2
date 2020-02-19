2a4656e105347d5af8055361eec59ba9de4dabe1
nikisalli <niki.salli@gmail.com>
2020-02-19 21:30:04 +0100

    added gauge but cannot get it to work on opengl3

--- software/nightmare_control/setup.py
+++ software/nightmare_control/setup.py
@@ -7,7 +7,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['nightmare_control'],
     package_dir={'': '/home/nik/catkin_ws/src'},
-    requires=['roslib', 'python_qt_binding', 'rospkg', 'rviz']
+    # requires=['roslib', 'python_qt_binding', 'rospkg', 'rviz']
+    requires=['roslib', 'python_qt_binding', 'rviz']
 )
 
 setup(**setup_args)
--- software/nightmare_control/src/nightcontrol.py
+++ software/nightmare_control/src/nightcontrol.py
@@ -1,22 +1,30 @@
 import sys
 import rviz
 import rospy
-from python_qt_binding import QtCore
-from python_qt_binding import QtGui
-from python_qt_binding import QtWidgets
-from python_qt_binding import QtQml
-from python_qt_binding import QtQuick
-from python_qt_binding import QtQuickWidgets
+import threading
+import os
+from std_msgs.msg import Float32
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5 import QtQml
+from PyQt5 import QtQuick
+from PyQt5 import QtQuickWidgets
 
 from radial_bar import RadialBar
 
-voltage = 7.2
+global voltage
+voltage = 6.9
 
-def listener_new():
- rospy.init_node('listener_new', anonymous=True)
- rospy.Subscriber("/nightmare/battery/voltage", String, callback)
- rospy.spin()
- 
+def listener():
+    rospy.init_node('nightcontrol_listener')
+    rospy.Subscriber("/nightmare/battery/voltage", Float32, callback)
+
+def callback(data):
+    global voltage
+    voltage = round(data.data,2)
+    
+    
 class MyClass(QtCore.QObject):
     randomValueChanged = QtCore.pyqtSignal(float)
     #self.randomValueChanged.emit(v)
@@ -48,14 +56,15 @@ class MyViz( QtWidgets.QWidget ):
         self.frame.initialize()
         reader = rviz.YamlConfigReader()
         config = rviz.Config()
-        reader.readFile( config, "/home/nik/Desktop/NightmareV2/software/nightmare_control/src/nightmare_config.rviz" )
+        path = dir_path = os.path.dirname(os.path.realpath(__file__))
+        reader.readFile( config, path + "/nightmare_config.rviz" )
         self.frame.load( config )
         self.setWindowTitle( config.mapGetChild( "Title" ).getValue() )
         self.frame.setMenuBar( None )
         self.frame.setStatusBar( None )
         self.frame.setHideButtonVisibility( False )
         self.manager = self.frame.getManager()
-        self.grid_display = self.manager.getRootDisplayGroup().getDisplayAt( 0 )
+        self.grid_display = self.manager.getRootDisplayGroup().getDisplayAt( 0 )        
         
         # voltage gauge widget
         self.batteryVWidget = QtQuickWidgets.QQuickWidget()
@@ -64,11 +73,9 @@ class MyViz( QtWidgets.QWidget ):
         # layout
         layout = QtWidgets.QHBoxLayout()
         
-        #hlayout = QtWidgets.QHBoxLayout()
         layout.addWidget( self.frame )
-        
-        #layout.addWidget( hlayout )
         layout.addWidget( self.batteryVWidget )
+        
         self.setLayout( layout )
         
 if __name__ == '__main__':
@@ -81,7 +88,8 @@ if __name__ == '__main__':
     batteryWidget = MyClass()
     
     myviz.batteryVWidget.rootContext().setContextProperty("batteryWidget",batteryWidget)
-    myviz.batteryVWidget.setSource(QtCore.QUrl.fromLocalFile('/home/nik/Desktop/NightmareV2/software/nightmare_control/src/qml_voltage_widget.qml'))
+    path = dir_path = os.path.dirname(os.path.realpath(__file__))
+    myviz.batteryVWidget.setSource(QtCore.QUrl.fromLocalFile(path + '/qml_voltage_widget.qml'))
     
     timer = QtCore.QTimer()
     timer.timeout.connect(batteryWidget.random_value)
@@ -89,5 +97,7 @@ if __name__ == '__main__':
     
     myviz.resize( 500, 500 )
     myviz.show()
-
-    app.exec_()
+    
+    listener()
+    sys.exit(app.exec_())
+    rospy.spin()
