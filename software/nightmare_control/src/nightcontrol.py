b2c84b29c1f861503003223eb33d5088637ccdf0
nikisalli <niki.salli@gmail.com>
2020-04-08 21:54:47 +0200

    fixed some graphics

--- software/nightmare_control/src/nightcontrol.py
+++ software/nightmare_control/src/nightcontrol.py
@@ -5,6 +5,9 @@ import os
 import pyqtgraph as pg
 import time
 import numpy as np
+import qdarkstyle
+import serial
+import threading
 
 from pyqtgraph import PlotWidget, plot
 from std_msgs.msg import Float32
@@ -13,6 +16,9 @@ from PyQt5 import QtGui
 from PyQt5 import QtWidgets
 
 from radial_bar import RadialBar
+from joystick import Joystick
+
+js = serial.Serial('/dev/ttyCOSO0', 115200)
 
 current_buf = []
 for _ in range(200):
@@ -23,7 +29,39 @@ for i in range(200):
     time_buf.append(i)
 
 buf = [current_buf, time_buf]
+buf_x1 = []
+buf_y1 = []
+buf_x2 = []
+buf_y2 = []
 
+def joystick_listener(viz):
+    while 1:
+        temp = js.read(2)
+        if(temp[0] == 0x55 and temp[1] == 0x55):
+            js.read(6)
+            while 1:
+                while(js.in_waiting >= 8):
+                    bytes = js.read(8)
+                    if(bytes[0] != 0x55 or bytes[1] != 0x55):
+                        break
+                    angle1 = fmap(bytes[2],0,255,0,360)
+                    angle2 = fmap(bytes[3],0,255,0,360)
+                    speed1 = fmap(bytes[4],0,100,0,1)
+                    speed2 = fmap(bytes[5],0,100,0,1)
+                    #button1 = bytes[6]
+                    #button2 = bytes[7]
+                    x1 = np.sin(np.radians(angle1))*speed1
+                    y1 = np.cos(np.radians(angle1))*speed1
+                    x2 = np.sin(np.radians(angle2))*speed2
+                    y2 = np.cos(np.radians(angle2))*speed2
+                        
+                    viz.draw_joystick(x1, x2, y1, y2)
+                time.sleep(0.01)
+            
+def fmap(x, in_min, in_max, out_min, out_max):                                                     #simple linear interpolation function
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min
+
+                
 def listener():
     rospy.init_node('nightcontrol_listener')
     rospy.Subscriber("/nightmare/battery/voltage", Float32, callbackV)
@@ -75,6 +113,14 @@ class MyViz( QtWidgets.QWidget ):
         self.batteryVWidget.textColor = QtGui.QColor(0,255,193)
         self.batteryVWidget.setFixedSize(250,250)
         
+        # right joystick
+        self.Rjoystick = Joystick()
+        self.Rjoystick.setMinimumSize( 350, 350 )
+        
+        # left joystick
+        self.Ljoystick = Joystick()
+        self.Ljoystick.setMinimumSize( 350, 350 )
+        
         # graph textbox
         self.textbox = QtWidgets.QLabel()
         self.textbox.setFixedSize(80,40)
@@ -92,12 +138,27 @@ class MyViz( QtWidgets.QWidget ):
         
         layout = QtWidgets.QHBoxLayout()
         
-        vlayout = QtWidgets.QVBoxLayout()
-        vlayout.addWidget( self.batteryVWidget, 0, QtCore.Qt.AlignHCenter )
-        vlayout.addWidget( self.graph )
+        vlayoutL1 = QtWidgets.QVBoxLayout()
+        vlayoutL1.addWidget( self.batteryVWidget, 0, QtCore.Qt.AlignHCenter )
+        vlayoutL1.addWidget( self.graph )
+        vlayoutL1.setAlignment( QtCore.Qt.AlignTop )
         
-        layout.addLayout(vlayout)
+        vlayoutL2 = QtWidgets.QVBoxLayout()
+        vlayoutL2.addWidget( self.Ljoystick )
+        vlayoutL2.setAlignment( QtCore.Qt.AlignBottom )
+        
+        vlayoutL = QtWidgets.QVBoxLayout()
+        vlayoutL.addLayout( vlayoutL1 )
+        vlayoutL.addLayout( vlayoutL2 )
+        vlayoutL.setAlignment( QtCore.Qt.AlignBottom )
+        
+        vlayoutR = QtWidgets.QVBoxLayout()
+        vlayoutR.addWidget( self.Rjoystick)
+        vlayoutR.setAlignment( QtCore.Qt.AlignBottom )
+        
+        layout.addLayout(vlayoutL)
         layout.addWidget( self.frame )
+        layout.addLayout(vlayoutR)
         
         self.setLayout(layout)
     
@@ -108,11 +169,20 @@ class MyViz( QtWidgets.QWidget ):
         pen = pg.mkPen(color=(0,255,193), width=4)
         self.graph.plot(buf[1], buf[0][-200:], clear=True, pen=pen)
     
+    def draw_joystick( self, x1, x2, y1, y2):
+        self.Rjoystick.setPos((y1*150), -(x1*150))
+        self.Ljoystick.setPos((y2*150), -(x2*150))
+    
 if __name__ == '__main__':
     app = QtWidgets.QApplication( sys.argv )
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
     
     myviz = MyViz()
+    myviz.resize(1920, 1067)
     myviz.show()
+    
+    thread = threading.Thread(target=joystick_listener, args=[myviz])
+    thread.start()
     
     timer = QtCore.QTimer()
     timer.timeout.connect(lambda: myviz.grab_data(buf))
